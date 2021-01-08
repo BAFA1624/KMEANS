2d3cf1e6095cbafa873792ca8107fe2616620352
Ben Andrews <benafandrews@gmail.com>
2021-01-08 00:26:04 +0000

    Fixed seg fault due to issue with the function ConcatArr.

--- static/ProcessFile.h
+++ static/ProcessFile.h
@@ -8,14 +8,15 @@ RowHolder* RawToRows(RawData* data);
 char* ConcatArr(char* arr, unsigned long idx_ini, unsigned long idx_fin);
 
 RowHolder* RawToRows(RawData* data) {
-	
-	char** rows = (char**)malloc( sizeof(char*) * data->rows );
-	int i, idx_ini, idx_fin, length, j = 0;
-	unsigned long* lens = (unsigned long*)malloc( sizeof(unsigned long) * data->rows );
 
-	for (i = 0; i < data->rows; ++i) {
+	// Allocating space for rows according to data in RawData object.	
+	char** rows = (char**)malloc( sizeof(char*) * data->n_rows );
+	int i, idx_ini, idx_fin, length, j = 0;
+	unsigned long* lens = (unsigned long*)malloc( sizeof(unsigned long) * data->n_rows );
+
+	for (i = 0; i < data->n_rows; ++i) {
 		
-		length = 0;
+		length = 1;
 		idx_ini = j;
 		while (data->txt[j++] != '\n') {
 			length++;
@@ -28,20 +29,19 @@ RowHolder* RawToRows(RawData* data) {
 	}
 	
 	RowHolder* result;
-	result->n_rows = data->rows;
+	result->n_rows = data->n_rows;
 	result->row_sizes = lens;
 	result->rows = rows;
-
-	free(lens);
 
 	return result;
 
 }
 
 char* ConcatArr(char* arr, unsigned long idx_ini, unsigned long idx_fin) {
-	unsigned long i, len = idx_fin - idx_ini + 1;
+	unsigned long i, len = idx_fin - idx_ini + 2;
 	char* result = (char*)malloc(len * sizeof(char));
-	result[idx_fin + 1] = '\0';
+	result[len-1] = '\0';
+	
 	for (i = idx_ini; i <= idx_fin; ++i) {
 		result[i - idx_ini] = arr[i];
 	}
--- static/test.c
+++ static/test.c
@@ -4,10 +4,20 @@ int main() {
 
 	RawData* data = ParseFile("data.txt");
 	RowHolder* test = RawToRows(data);
+/*	
+	char* txt = data->txt;
+
+	char* test = ConcatArr(txt, 40, 43);
+
+	printf("\n%c%c%c%c\n", txt[40], txt[41], txt[42], txt[43]);
+	printf("\n%s\n", test);
+*/
 
 	int i;
-	for (i = 0; i < 1; ++i) {
-		printf("\n%s\nn_rows = %lu\n", test->rows[i], test->n_rows);
+	for (i = 0; i < 5; ++i) {
+		printf("\n%s\n", test->rows[i]);
 	}
+
+	delRawData(data);
 
 }
